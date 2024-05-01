516be75e4fbcffd31b2e995b7b7cd11bf18e4939
ericflavio <ericflavio@gmail.com>
2024-05-01 18:34:01 -0300

    melhorias

--- src/app/(tabs)/login.js
+++ src/app/(tabs)/login.js
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
+//efa
+import React, { useState } from 'react';
+import { View, Text, Image, SafeAreaView, ScrollView, TouchableOpacity, Dimensions } from "react-native";
+import { myStyles } from "../styles/stylesPageIndex";
+import { myStylesComuns } from '../styles/stylesComuns';
+import { GradienteFill } from '../componentes/gradienteFill';
+
+//Tela principal
 export default function ViewLogin() {
+ 
+  //Seta tamanho ideal das VIEWS baseado no tamanho da tela do aparelho
+  let screenHeight = 0;
+  try {
+    screenHeight = Dimensions.get('window').height;
+  } catch {
+    screenHeight = myStylesComuns.containerTamanhoMedioTelas;
+  };
+  let headerHeight = 0; //Eventual header na página
+  let bodyHeight = screenHeight - headerHeight + 0;
+  //console.log("screenHeight: ", screenHeight);
+  //console.log("headerHeight: ", headerHeight);
+  //console.log("bodyHeight  : ", bodyHeight);
+
   return (
-    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-      <Text style={{fontSize:44 , fontWeight:"700"}}>
-        Login
-      </Text>
-    </View>
+    <SafeAreaView style={myStylesComuns.containerPrincipalSafeArea}>
+      {GradienteFill()}
+      <ScrollView style={myStylesComuns.containerPrincipalScroll} showsVerticalScrollIndicator={false}>
+
+        <View style={{ height: bodyHeight, paddingBottom: 96 }}>
+          <View style={myStyles.containerHeader}>
+            <Text style={myStylesComuns.textoTituloPagina}>
+              login
+            </Text>
+          </View>
+          <View style={myStyles.containerBody1}>
+            <Text>Body1</Text>
+          </View>
+
+          <View style={myStyles.containerBody2}>
+           <Texto>Body2</Texto>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView >
   )
 }
---
+++ src/app/(tabs)/login_bkp.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+export default function ViewLogin() {
+  return (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+      <Text style={{fontSize:44 , fontWeight:"700"}}>
+        Login
+      </Text>
+    </View>
+  )
+}
