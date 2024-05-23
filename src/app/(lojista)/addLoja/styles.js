bcb8a51c7280f83456f60dd10d6d38c83e319467
ericflavio <ericflavio@gmail.com>
2024-05-23 19:56:55 -0300

    melhorias

---
+++ src/app/(lojista)/addLoja/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+import { myStylesComuns } from "../../../styles/stylesComuns";
+
+export const myStyles = StyleSheet.create({
+  containerHeader: {
+    flex:0,
+    borderWidth: myStylesComuns.containerBordaOnOff
+  },
+  conteinerTextoAvatar: {
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    minHeight: 54,
+    borderWidth: myStylesComuns.containerBordaOnOff
+  },
+  containerRecepcionista: {
+    flex:0,
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 16,
+    padding: 8,
+    borderWidth: myStylesComuns.containerBordaOnOff
+  },
+  containerAvatar: {
+    flex:0,
+    flexDirection: "row",
+    alignItems: "stretch",
+    borderWidth: myStylesComuns.containerBordaOnOff
+  },
+  containerFacilidades: {
+    flex:0,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginTop: 20,
+    borderWidth:0,
+  },
+  imgAvatar: {
+    width: 54, 
+    height: 54, 
+    resizeMode: 'contain',
+  },
+});
--- src/app/(main)/conta/index.js
+++ src/app/(main)/conta/index.js
@@ -81,6 +81,7 @@ export default function ViewContaGestao() {
             </View>
           </TouchableOpacity>
         </View>
+
       </ScrollView>
     </SafeAreaView >
   )
