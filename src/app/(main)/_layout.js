35c4f1aed9835b4ac44497d05ea8247ce0433cfc
ericflavio <ericflavio@gmail.com>
2024-06-15 23:18:17 -0300

    m

--- src/app/(main)/_layout.js
+++ src/app/(main)/_layout.js
@@ -60,29 +60,30 @@ export default function AppLayout() {
 
   //Usuário logado e status ativo. Monta o layout do app
   return (
-    <Tabs screenOptions={{
-      headerShown: false,
-      tabBarInactiveTintColor: styleColor.cinzaMedio,
-      tabBarActiveTintColor: styleColor.tema30pPrincipal,
-      tabBarShowLabel: false,
-      tabBarLabelStyle: {
-        fontSize: 16,
-        paddingBottom: 6,
-      },
-      tabBarStyle: {
-        position: "absolute",
-        backgroundColor: styleColor.tema60pClaro,
-        borderTopWidth: 0,
-        borderTopLeftRadius: 16,
-        borderTopRightRadius: 16,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        elevation: 0,
-        height: 74,
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarInactiveTintColor: styleColor.cinzaMedio,
+        tabBarActiveTintColor: styleColor.tema30pPrincipal,
+        tabBarShowLabel: false,
+        tabBarLabelStyle: {
+          fontSize: 16,
+          paddingBottom: 6,
+        },
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: styleColor.tema60pClaro,
+          borderTopWidth: 0,
+          borderTopLeftRadius: 16,
+          borderTopRightRadius: 16,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          height: 74,
+        },
       }
-
-    }}>
+      }>
 
       <Tabs.Screen
         name="index"
@@ -110,6 +111,6 @@ export default function AppLayout() {
             <MaterialIcons name="person" size={styleApp.size.iconSizeTabBar} color={color} />
         }}
       />
-    </Tabs>
+    </Tabs >
   )
 }
